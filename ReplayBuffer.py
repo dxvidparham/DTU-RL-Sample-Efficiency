635c8b68971da3d14d81ad12f2e35fac3b18ab63
lassestarklit <s165498@student.dtu.dk>
2020-11-23 15:12:44 +0100

    added replay buffer

---
+++ ReplayBuffer.py
@@ -0,0 +1,12 @@
+
+
+class ReplayBuffer(object):
+    def __init__(self,capacity):
+        NotImplemented
+
+    def add(self, obs, action, reward, next_obs, done, done_no_max):
+        NotImplemented
+
+    def sample(self, batch_size):
+        NotImplemented
+
