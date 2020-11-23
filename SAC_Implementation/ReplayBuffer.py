28897a70adb341e3d583f5ab9543268ebce96053
lassestarklit <s165498@student.dtu.dk>
2020-11-23 17:04:53 +0100

    implemented replay buffer

--- SAC_Implementation/ReplayBuffer.py
+++ SAC_Implementation/ReplayBuffer.py
@@ -4,9 +4,16 @@ import torch
 
 class ReplayBuffer(object):
 
-    def __init__(self, capacity):
+    def __init__(self, obs_shape, action_shape, capacity):
         self.capacity = capacity
-        self.memory = np.empty([capacity, 6])
+
+        self.obs = np.empty(capacity, *obs_shape)
+        self.next_obs = np.empty(capacity, *obs_shape)
+        self.action = np.empty(capacity, *action_shape)
+        self.reward = np.empty(capacity,1)
+        self.not_done = np.empty(capacity, 1)
+        self.not_done_no_max = np.empty(capacity,1)
+
         self.idx = 0
         self.last_save = 0
         self.full = False
@@ -16,7 +23,14 @@ class ReplayBuffer(object):
         return self.capacity if self.full else self.idx
 
     def add(self, obs, action, reward, next_obs, done, done_no_max=0):
-        self.memory[self.idx] = np.array([obs, action, reward, next_obs, not done, not done_no_max])
+
+        self.obs[self.idx] = obs
+        self.next_obs[self.idx] = next_obs
+        self.action[self.idx] = action
+        self.reward[self.idx] = reward
+        self.not_done[self.idx] = done
+        self.not_done_no_max[self.idx] = done_no_max
+
         self.idx = (self.idx + 1) % self.capacity
 
         self.full = self.full or self.idx == 0
@@ -26,16 +40,16 @@ class ReplayBuffer(object):
                                  self.capacity if self.full else self.idx,
                                  size=batch_size)
 
-        obses = torch.as_tensor(self.memory[idxs][:,0]).float()
+        obses = torch.as_tensor(self.obs[idxs]).float()
 
-        actions = torch.as_tensor(self.memory[idxs][:,1])
+        actions = torch.as_tensor(self.action[idxs])
 
-        rewards = torch.as_tensor(self.memory[idxs][:,2])
+        rewards = torch.as_tensor(self.reward[idxs])
 
-        next_obses = torch.as_tensor(self.memory[idxs][:,3]).float()
+        next_obses = torch.as_tensor(self.next_obs[idxs]).float()
 
-        not_dones = torch.as_tensor(self.memory[idxs][:,4])
-        not_dones_no_max = torch.as_tensor(self.memory[idxs][:,5])
+        not_dones = torch.as_tensor(self.not_done[idxs])
+        not_dones_no_max = torch.as_tensor(self.not_done_no_max[idxs])
 
         return obses, actions, rewards,next_obses, not_dones, not_dones_no_max
 
