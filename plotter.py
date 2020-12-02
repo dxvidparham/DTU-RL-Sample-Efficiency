895e170d2baff18d711ba8c6d995f708b274aa35
david <s202385@student.dtu.dk>
2020-12-02 18:58:03 +0100

    get current version

--- plotter.py
+++ plotter.py
@@ -35,9 +35,14 @@ class Plotter:
         #plt.xlabel('Iteration Number', fontsize=25, labelpad=-4)
         plt.subplot(411)
         plt.title('training rewards')
+<<<<<<< HEAD
+        plt.plot(range(1, _eps + 1), self.rewards, label='hello')
+        plt.plot(moving_average(self.rewards))
+=======
         plt.plot(range(1, _eps + 1), self.rewards, label="Model")
         plt.plot(moving_average(self.rewards), label="Moving Average")
         plt.legend()
+>>>>>>> 8cca2b44f5a5e395c9c55a541a0ba05ba856da39
         plt.xlim([0, _eps])
         plt.subplot(412)
         plt.title('training lengths')
@@ -56,8 +61,12 @@ class Plotter:
         plt.xlim([0, _eps])
 
         plt.tight_layout()
+<<<<<<< HEAD
+        now = datetime.now().strftime("%d%m%Y%H%M%S")
+=======
 
         now = datetime.now().strftime("%d_%m_%Y_-%H_%M_%S")
+>>>>>>> 8cca2b44f5a5e395c9c55a541a0ba05ba856da39
         filename = f"plot_{now}.png"
         plt.savefig('figures/' + filename)
 
